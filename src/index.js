048f8f03ad8bd960ac53b8ba3e14dc1ac4b86018
Deb-das01 <97828828+Deb-das01@users.noreply.github.com>
2024-01-22 01:50:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+// Use ReactDOM.createRoot
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
-
 reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+// reportWebVitals();
